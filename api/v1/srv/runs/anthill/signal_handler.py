a3f137cc35fda43d687a5c8bad69c7ed9a144e65
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-23 17:50:49 +0300

    добавил created_at и updated_at

--- api/v1/srv/runs/anthill/signal_handler.py
+++ api/v1/srv/runs/anthill/signal_handler.py
@@ -1,16 +1,21 @@
 import uuid
+from datetime import datetime
 
 def processing_signal(data: dict) -> dict:
     status = data["status"]
     start_time = data["start_time"]
     run_id = str(uuid.uuid4())
     job_id = str(uuid.uuid4())
+    created_at = datetime.utcnow().isoformat()
+    updated_at = datetime.utcnow().isoformat() if "updated_at" in data else created_at
 
     result = {
         "run_id": run_id,
         "job_id": job_id,
         "status": status,
-        "start_time": start_time
+        "start_time": start_time,
+        "created_at": created_at,
+        "updated_at": updated_at
     }
  
     return result
