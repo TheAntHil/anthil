c919b88eb53f6de9ef6deb8a2efbdab32bdb20cf
Semen Pavelko <semen.pavelko@x5.ru>
2025-03-29 01:41:25 +0300

    feat: add system post

--- anthill/models.py
+++ anthill/models.py
@@ -23,3 +23,28 @@ class RunModel(Base):
                 f'{self.created_at}\n'
                 f'{self.updated_at}\n'
                 f'{self.start_time}>')
+
+
+class SystemModel(Base):
+    __tablename__ = 'systems'
+
+    system_id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(String(50), nullable=False)
+    url: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    token: Mapped[str] = mapped_column(String(255), nullable=False)
+    system_type: Mapped[str] = mapped_column(String(50), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return (f'<System\n'
+                f'system_id={self.system_id}\n'
+                f'code={self.code}\n'
+                f'url={self.url}\n'
+                f'token={self.token}\n'
+                f'system_type={self.system_type}\n'
+                f'created_at={self.created_at}\n'
+                f'updated_at={self.updated_at}>')
--- anthill/server.py
+++ anthill/server.py
@@ -3,8 +3,8 @@ import logging
 from anthill.signal_handler import process_signal, convert
 from anthill.run_handler import filter_runs, sort_runs
 from datetime import datetime as dt
-from anthill.queries import insert_run, get_runs_by_db
-
+from anthill.queries import insert_run, get_runs_by_db, insert_system
+from anthill.system_handler import convert_to_obj, convert_to_dict
 
 logging.basicConfig(
     level=logging.INFO,
@@ -44,6 +44,20 @@ def get_runs():
         converted_runs = [convert(run) for run in db_runs]
         logger.info(f"Filtered and sorted fetched {len(converted_runs)} runs.")
         return jsonify(converted_runs)
+    except Exception as e:
+        logger.error(f"Error processing request: {e}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/v1/admin/systems/', methods=['POST'])
+def create_system():
+    system_json = request.get_json()
+    logger.info(f"Received data: {system_json}")
+    try:
+        prepared_system = convert_to_obj(system_json)
+        logger.info(f"Processing result: {prepared_system}")
+        answer = insert_system(prepared_system)
+        answer_json = convert_to_dict(answer)
+        return jsonify(answer_json), 201
     except Exception as e:
         logger.error(f"Error processing request: {e}")
         return jsonify({"error": str(e)}), 500
