3306e167fc5adc9ff862d680dd47f1216a80b6f4
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-28 17:31:40 +0300

    get_runs теперь работает с бд

--- anthill/queries.py
+++ anthill/queries.py
@@ -2,6 +2,8 @@ from anthill.models import RunModel
 from anthill.db import get_session
 from anthill.signal_handler import Run
 import logging
+from sqlalchemy import select
+from datetime import datetime as dt
 
 
 logger = logging.getLogger(__name__)
@@ -20,9 +22,26 @@ def insert_run(prepared_run: Run) -> None:
     try:
         session.add(run_model)
         session.commit()
-        logger.info(f"QUERY  Record successfully inserted.")
+        logger.info("QUERY  Record successfully inserted.")
     except Exception as e:
         session.rollback()
         logger.error(f"QUERY Error: {e}")
     finally:
         session.close()
+
+
+def get_runs_by_db(after, sort) -> None:
+    session = get_session()
+    query = select(RunModel).where(RunModel.updated_at > after)
+    sort_field = getattr(RunModel, sort)
+    query = query.order_by(sort_field)
+    result = session.execute(query)
+    runs = result.scalars().all()
+    return [Run(
+        run_id=run.run_id,
+        job_id=run.job_id,
+        status=run.status,
+        start_time=run.start_time,
+        created_at=run.created_at,
+        updated_at=run.updated_at
+    ) for run in runs]
