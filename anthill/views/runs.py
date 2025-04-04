92b5cb0f4f80460e00a727a8f0d508c7d05cfb4e
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-04 14:07:32 +0300

    Перенос Run

--- anthill/views/runs.py
+++ anthill/views/runs.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify
 from datetime import datetime
 import logging
-from anthill import run_handler, queries
+from anthill import run_handler, db
+from anthill.repos import runs
 
 view = Blueprint('runs', __name__, url_prefix='/api/v1/srv/runs')
 logger = logging.getLogger(__name__)
+session = db.get_session()
+run_repo = runs.RunRepo(session)
 
 
 @view.route("/", methods=["POST"])
@@ -14,7 +17,7 @@ def create_run():
     try:
         prepared_run = run_handler.process_run(run)
         logger.info(f"Processing result: {prepared_run}")
-        queries.insert_run(prepared_run)
+        run_repo.insert(prepared_run)
         converted_run = run_handler.convert(prepared_run)
         return jsonify(converted_run), 201
     except Exception as e:
@@ -27,7 +30,7 @@ def get_runs():
     after = datetime.fromisoformat(request.args.get("after").replace(" ", "+"))
     logger.info(f"Received request, parameters: after={after}")
     try:
-        db_runs = queries.get_runs(after)
+        db_runs = run_repo.get(after)
         converted_runs = [run_handler.convert(run) for run in db_runs]
         logger.info(f"Filtered and sorted fetched {len(converted_runs)} runs.")
         return jsonify(converted_runs)
