8d543a4c202e2eafb37435649b77bafcc9c84417
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-18 21:55:08 +0300

    коммент

--- anthill/models.py
+++ anthill/models.py
@@ -7,15 +7,15 @@ import enum
 
 
 class RunStatus(enum.Enum):
-    created = "created"
-    scheduled = "scheduled"
-    triggered = "triggered"
+    CREATED = "created"
+    SCHEDULED = "scheduled"
+    TRIGGERED = "triggered"
 
 
 class ScheduledStatus(enum.Enum):
-    scheduled = "scheduled"
-    triggered = "triggered"
-    cancelled = "cancelled"
+    SCHEDULED = "scheduled"
+    TRIGGERED = "triggered"
+    CANCELLED = "cancelled"
 
 
 class Run(Base):
@@ -30,7 +30,7 @@ class Run(Base):
     status: Mapped[RunStatus] = mapped_column(
         Enum(RunStatus, name="run_status"),
         nullable=False,
-        default=RunStatus.created)
+        default=RunStatus.CREATED)
 
     def __repr__(self):
         return (f'<RunModel\n'
@@ -136,7 +136,7 @@ class ScheduledTask(Base):
     status: Mapped[ScheduledStatus] = mapped_column(
         Enum(ScheduledStatus, name="scheduled_status"),
         nullable=False,
-        default=ScheduledStatus.scheduled)
+        default=ScheduledStatus.SCHEDULED)
 
     def __repr__(self):
         return (f'<Scheduled\n'
