510ec622077129bde1cb89d14f29d5477cad2999
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-26 15:12:00 +0300

    статус меняется

--- anthill/repos/scheduleds.py
+++ anthill/repos/scheduleds.py
@@ -1,10 +1,10 @@
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime, UTC
 import logging
-from anthill import models
+from anthill.models import ScheduledStatus, ScheduledTask
 from typing import Sequence
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 logger = logging.getLogger(__name__)
 
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 class ScheduledRepo:
     def add(self, session: Session, job_id: int,
             scheduled_at: datetime,
-            status: models.ScheduledStatus) -> models.ScheduledTask:
-        scheduled_model = models.ScheduledTask(
+            status: ScheduledStatus) -> ScheduledTask:
+        scheduled_model = ScheduledTask(
             job_id=job_id,
             scheduled_at=scheduled_at,
             status=status
@@ -30,9 +30,25 @@ class ScheduledRepo:
         return scheduled_model
 
     def get_scheduled_tasks(
-            self, session: Session) -> Sequence[models.ScheduledTask]:
-        query = select(models.ScheduledTask).where(
-            models.ScheduledTask.status == models.ScheduledStatus.SCHEDULED)
+            self, session: Session) -> Sequence[ScheduledTask]:
+        query = select(ScheduledTask).where(
+            ScheduledTask.status == ScheduledStatus.SCHEDULED)
         result = session.execute(query)
         scheduled_tasks = result.scalars().all()
         return scheduled_tasks
+
+    def update_status_task(self, session: Session,
+                           scheduled_task_id: int) -> None:
+        query = update(ScheduledTask)
+        query = query.where(ScheduledTask.scheduled_id == scheduled_task_id)
+        query = query.values(status=ScheduledStatus.TRIGGERED,
+                             scheduled_at=datetime.now(tz=UTC))
+        session.execute(query)
+        session.commit()
+
+    def get_task_by_id(self, task_id: int, session) -> ScheduledTask:
+        query = select(ScheduledTask)
+        query = query.where(ScheduledTask.scheduled_id == task_id)
+        result = session.execute(query)
+        task = result.scalars().one_or_none()
+        return task
