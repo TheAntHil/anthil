665aa673eb0c3e73a599a6a57f8518b740844cde
Sergey Simakov <SaneniRem@yandex.ru>
2025-05-08 20:18:59 +0300

    12

--- anthill/repos/jobs.py
+++ anthill/repos/jobs.py
@@ -1,8 +1,8 @@
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session, aliased
-from sqlalchemy import select
-from sqlalchemy.sql.functions import coalesce
-from datetime import datetime, timedelta
+from sqlalchemy import select, distinct
+from sqlalchemy.sql.functions import coalesce, count
+from datetime import datetime, timedelta, UTC
 import logging
 from anthill import models
 from typing import Sequence
@@ -44,16 +44,31 @@ class JobRepo:
         return job_model
 
     def get_schedule_jobs_for_run(self, run_id: UUID,
-                                  session: Session) -> Sequence[models.Job]:
+                                  session: Session) -> Sequence[tuple[int, int]]:
         CompletedRun = aliased(models.Run)
         DependesRun = aliased(models.Run)
-        query = select(models.Job).join(
-            models.Dependence,
-            models.Dependence.trigger_job_id == models.Job.job_id,
+        OtherRun = aliased(models.Run)
+
+        DependenceOne = aliased(models.Dependence)
+        DependenceTwo = aliased(models.Dependence)
+        
+        actuality_time = datetime.now(tz=UTC) - timedelta(minutes=1)
+        query = select(models.Job.job_id, count(distinct(OtherRun.job_id))).join(
+            DependenceOne,
+            DependenceOne.trigger_job_id == models.Job.job_id,
         ).join(
             CompletedRun,
-            models.Dependence.completed_job_id == CompletedRun.job_id,
+            DependenceOne.completed_job_id == CompletedRun.job_id,
         ).where(CompletedRun.run_id == run_id)
+        
+        query = query.join(
+            DependenceTwo,
+            DependenceTwo.trigger_job_id == models.Job.job_id   
+        )
+        query = query.join(
+            OtherRun,
+            DependenceTwo.completed_job_id == OtherRun.job_id,
+        ).where(OtherRun.updated_at > actuality_time)
 
         query = query.join(
             DependesRun,
@@ -61,12 +76,15 @@ class JobRepo:
         ).where(
             coalesce(
                 DependesRun.updated_at,
-                datetime(1970, 1, 1)) < datetime.now() - timedelta(minutes=1),
+                datetime(1970, 1, 1)) < actuality_time,
         )
+
+        query = query.group_by(models.Job.job_id)
+
         logger.debug(
             f"QUERY: JobRepo.get_schedule_jobs_for_run — run_id={run_id}")
         result = session.execute(query)
-        jobs = result.scalars().all()
+        jobs = result.all()
         return jobs
 
     def get_jobs_by_id(self, job_id: int,
