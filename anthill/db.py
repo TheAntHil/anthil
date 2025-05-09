171a543089d8bb0b4a534dbc2f36c818c8d4604f
Sergey Simakov <SaneniRem@yandex.ru>
2025-05-09 11:32:00 +0300

    echo правка

--- anthill/db.py
+++ anthill/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from anthill import config
 
 
-engine = create_engine(config.get_db_url(), echo=True)
+engine = create_engine(config.get_db_url())
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
